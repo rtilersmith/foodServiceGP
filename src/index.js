6f1ef92f484207181044dab43117df5b9ac9ecf6
Tiler Smith <xanderzmydog@msn.com>
2018-10-09 10:25:54 -0600

    hosting prep

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 //components
-import { HashRouter as Router } from 'react-router-dom'
+import { BrowserRouter as Router } from 'react-router-dom'
 //creating an alias for HashRouter to change into {BrowserRouter} at a later time
 import { Provider } from 'react-redux'
 import store from './Redux/store'
